ad0e2a1e046d39ef627765c4f682829a1496ae47
Theo Lipeles <Theo@Lipeles.com>
2018-11-07 18:30:08 -0500

    adds basic query route

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,4 +1,11 @@
 const express = require('express');
 const router = new express.Router();
+const path = require('path');
+const api = require(path.join(__dirname, '../api'));
+
+router.get('/query', (req, res, next) => {
+  api.getResults(req.body.query, req.body.pagination)
+      .then((results) => res.send);
+});
 
 module.exports = router;
