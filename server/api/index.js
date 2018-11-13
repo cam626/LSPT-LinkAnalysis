a7418b0fe2e38fe45e0dac7beb86734f6f3f32de
Theo Lipeles <Theo@Lipeles.com>
2018-11-13 18:45:53 -0500

    fixes linter errors

--- server/api/index.js
+++ server/api/index.js
@@ -1,35 +1,36 @@
 const http = require('http');
-const rankingURL = "http://blender01.cs.rpi.edu:8080/ranking?query=";
-const indexingURL = "";
+const rankingURL = 'http://blender01.cs.rpi.edu:8080/ranking?query=';
+const indexingURL = '';
 
 /** Gets the results JSON for a specific query form ranking
  *  @param {String} query - the query the user searched for
- *  @param {requestCallback} callback - called with an Object containing 
-                    a pages array 
+ *  @param {requestCallback} callback - called with an Object containing a pages
+ *  array
  */
 function getDocIds(query, callback) {
   let parsedData;
   http.get(rankingURL + query, (resp) => {
     const status = resp.statusCode;
-    
-    if(status != 200){
-      let error = new Error("Request to ranking was unsuccessful\n");
+
+    if (status != 200) {
+      const error = new Error('Request to ranking was unsuccessful\n');
       console.error(error.message);
       resp.resume();
-      return callback( { docs: [] } );
+      return callback( {docs: []} );
     }
 
     let rawData = '';
-    resp.on('data', (chunk) => { rawData += chunk; });
+    resp.on('data', (chunk) => {
+      rawData += chunk;
+    });
     resp.on('end', () => {
-      try{
+      try {
         parsedData = JSON.parse(rawData);
 
         return callback( parsedData );
-      }
-      catch(e){
+      } catch (e) {
         console.error(e.message);
-        return callback( { docs: [] } );
+        return callback( {docs: []} );
       }
     });
   });
@@ -37,19 +38,19 @@ function getDocIds(query, callback) {
 
 /** Gets an array of pages JSONs from indexing
  *  @param {Array} docIds - an array of integers
- *  @param {requestCallback} callback - called with an array 
+ *  @param {requestCallback} callback - called with an array
                     containing page information
  */
-function getPages(docIds, callback){
-  let results = [];
-  for(const key in docIds){
-    if(!docIds.hasOwnProperty(key)){
+function getPages(docIds, callback) {
+  const results = [];
+  for (const key in docIds) {
+    if (!docIds.hasOwnProperty(key)) {
       continue;
     }
     http.get(indexingURL + key, (resp) =>{
       const status = resp;
-      if(status != 200){
-        let error = new Error("Request to indexing was unsuccessful\n");
+      if (status != 200) {
+        const error = new Error('Request to indexing was unsuccessful\n');
         console.error(error.message);
         resp.resume();
         callback( [] );
@@ -57,19 +58,17 @@ function getPages(docIds, callback){
 
       let rawData = '';
       resp.on('data', (chunk) => {
-        rawData += chunk; 
+        rawData += chunk;
       });
 
       resp.on('end', () => {
-        try{
+        try {
           const parsedData = JSON.parse(rawData);
-          results.append(rawData);
-        }
-        catch(e){
+          results.append(parsedData);
+        } catch (e) {
           console.error(e.message);
         }
       });
-
     });
   }
 
