346ccd7d4cde302c69d4015303c6246d51034c57
RioMichael <lix23@rpi.edu>
2018-11-14 18:18:01 -0500

    Add functions related to rank
    Add function for getting rank for all nodes, return a map.
    Add function for updating the rank for the graph (current parameter: d = 0.85, threshold = 0.1%)

--- Webgraph.cpp
+++ Webgraph.cpp
@@ -5,6 +5,7 @@
 
 #include "Node.h"
 #include "Webgraph.h"
+#include <queue>
 
 typedef unsigned int uint;
 
@@ -40,6 +41,14 @@ const std::vector<Node>& Webgraph::getOutgoingNodes(const Node &n) const {
     return n.getChildren();
 }
 
+const std::map< std::string, float>& getAllRanks() const{
+    std::map< std::string, float > all_ranks;
+    for (int i=0; i<all_links.size(); ++i){
+        all_ranks[all_links[i].getUrl()] = all_links[i].getRank();
+    }
+    return all_ranks;
+}
+
 bool Webgraph::addLink(const string &url_) {
     if (hasLink(url_)) { return false; }
     Node n(url_);
@@ -65,4 +74,56 @@ bool Webgraph::addConnection(const std::string &from_url,
     if (added == false) { return false; }
     adj_matrix[n_from].push_back(n_to);
     return true;
+}
+
+
+/inital the rank update, assume url exists
+void Webgraph::updateRank(const string &url_) {
+    //inital the queue and start the iterative update
+    queue<Node> work_queue;
+    Node start = getNode(url_);
+    work_queue.push(start);
+    updateHelper(work_queue);
+    return;
+}
+
+//actual rank update function, iterative update the 
+//rank of the nodes until the queue is empty
+void Webgraph::updateHelper(queue<Node> work_queue){
+    //finish condition
+    if (work_queue.empty()){
+        return;
+    }
+
+    //update the rank of the node that is on the top
+    //of the queue
+    Node n_current = work_queue.pop()
+    //calculate new rank based on incoming nodes
+    vector<Node> parent = getIncomingNodes(n_current);
+    float new_rank = 0;
+    for (int i=0; i<parent.size(); ++i){
+        Node n_from = from_list[i];
+        float from_rank = n_from.getRank();
+        int outgoing_number = getOutgoingNodes(n_from).size();
+        new_rank += from_rank/outgoing_number;
+    }
+    new_rank = 0.15 + 0.85*new_rank;
+    old_rank = n_current.getRank();
+    //update the rank
+    n_current.updateRank(new_rank);
+
+    //check converge, threshold current set to 0.1% 
+    if (abs(new_rank - old_rank)/old_rank < 0.001){
+        //converged, current node's children would not add to queue
+        updateHelper(work_queue);
+    }
+    else{
+        //not converged, all current node's children to queue
+        vector<Node> children = getOutgoingNodes(n_current);
+        for (int i=0; i<children.size(); ++i) {
+            work_queue.push(children[i])
+        }
+        updateHelper(work_queue);
+    }
+    return;
 }
