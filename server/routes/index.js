7603254c57dd8bd15cf97feca3f9392374f93459
Theo Lipeles <Theo@Lipeles.com>
2018-11-15 10:36:11 -0500

    adds rudimentary query route

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const router = new express.Router();
 const path = require('path');
-const api = require(path.join(__dirname, '../api'));
+const api = require(path.join(__dirname, '..', '/api'));
 
-router.get('/query', (req, res, next) => {
-  api.getResults(req.body.query, req.body.pagination)
-      .then((results) => res.send);
+router.get('/query', (req, res) => {
+  api.getDocIds(req.body.query, (docObj) => {
+    const docIds = [];
+    for (let i = 0; i < docObj.docs; i++) {
+      docIds.push(docObj[i].docid);
+    }
+    api.getPages(docIds, res.send);
+  });
 });
 
 module.exports = router;
