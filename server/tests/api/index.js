a60165464bd9426db93bb13d7ad4bc328c4e6f28
AJowett <andrewwjowett@gmail.com>
2018-11-15 10:46:05 -0500

    adds more test cases for getDocIds

--- server/tests/api/index.js
+++ server/tests/api/index.js
@@ -6,9 +6,14 @@ const apis = require(path.join(__dirname, '..', '..', 'api'));
 /** Tests that getDocIds yeilds documents */
 /*
 function testGetDocIds() {
-  apis.getDocIds("", (resp) => {
-      assert(resp.docs);
-      assert(resp.docs.length >= 0);
+  apis.getDocIds('', (resp) => {
+    assert(resp.docs);
+    assert(resp.docs.length >= 0);
+  });
+    
+  apis.getDocIds('test test' (resp) {
+    assert(resp.docs);
+    assert(resp.docs.length >= 0);
   });
 
   apis.getDocIds('test', (resp) => {
