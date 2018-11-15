62e922197f9e5c22f7508d770082078301544c8e
AJowett <andrewwjowett@gmail.com>
2018-11-15 11:00:12 -0500

    fixes lint issue

--- server/api/index.js
+++ server/api/index.js
@@ -42,7 +42,7 @@ function getDocIds(query, callback) {
                     containing page information
  */
 function getPages(docIds, callback) {
-  let results = [];
+  const results = [];
   for (const key in docIds) {
     if (!docIds.hasOwnProperty(key)) {
       continue;
