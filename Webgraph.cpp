7c0dee6dfbed00266057a2df635d11a81537549f
RioMichael <lix23@rpi.edu>
2018-11-15 01:59:32 -0500

    Updated several function to perform correct rank update

--- Webgraph.cpp
+++ Webgraph.cpp
@@ -8,6 +8,7 @@
 #include <queue>
 #include <cmath>
 
+
 typedef unsigned int uint;
 
 using namespace std;
@@ -41,6 +42,20 @@ const Node &Webgraph::getNodeFromLink(const std::string &url_) const
         }
     }
     return all_nodes[i];
+}
+
+// Assume the node with the provided url exists
+uint Webgraph::getNodeIndexFromLink(const std::string &url_) const
+{
+    uint i = 0;
+    for (; i < all_nodes.size(); ++i)
+    {
+        if (all_nodes[i].getUrl() == url_)
+        {
+            break;
+        }
+    }
+    return i;
 }
 
 // Assume the provided node exists
@@ -113,16 +128,16 @@ bool Webgraph::addConnection(const std::string &from_url,
         addLink(to_url);
     }
     // update Node info
-    Node n_from = getNodeFromLink(from_url);
-    Node n_to = getNodeFromLink(to_url);
-    bool added = n_from.addChild(n_to);
-    n_to.addParent(n_from);
+    uint index_from = getNodeIndexFromLink(from_url);
+    uint index_to = getNodeIndexFromLink(to_url);
+    bool added = all_nodes[index_from].addChild(all_nodes[index_to]);
+    all_nodes[index_to].addParent(all_nodes[index_from]);
     // update adj matrix
     if (added == false)
     {
         return false;
     }
-    adj_matrix[n_from].push_back(n_to);
+    adj_matrix[all_nodes[index_from]].push_back(all_nodes[index_to]);
     return true;
 }
 
@@ -135,9 +150,9 @@ bool Webgraph::addConnection(const std::string &from_url,
 void Webgraph::updateRank(const string &url_)
 {
     //inital the queue and start the iterative update
-    queue<Node> work_queue;
-    Node start = getNodeFromLink(url_);
-    work_queue.push(start);
+    queue<uint> work_queue;
+    uint index_start = getNodeIndexFromLink(url_);
+    work_queue.push(index_start);
     updateHelper(work_queue);
     return;
 }
@@ -147,38 +162,35 @@ void Webgraph::updateRank(const string &url_)
     rank of the nodes until the queue is empty
 
     Parameters:
-        queue<Node> work_queue: The queue that store the URL
-                                nodes that need to be updated
+        queue<uint> work_queue: The queue that store the URL
+                                nodes' index that need to be updated
 */
-void Webgraph::updateHelper(queue<Node> work_queue)
+void Webgraph::updateHelper(queue<uint> work_queue)
 {
     //finish condition
     if (work_queue.empty())
     {
         return;
     }
-
     //update the rank of the node that is on the top
     //of the queue
-    Node n_current = work_queue.front();
+    uint index_current = work_queue.front();
     work_queue.pop();
     //calculate new rank based on incoming nodes
-    vector<Node> parent = getIncomingNodes(n_current);
+    vector<Node> parent = getIncomingNodes(all_nodes[index_current]);
     float new_rank = 0;
     for (uint i = 0; i < parent.size(); ++i)
     {
-        Node n_from = parent[i];
-        float from_rank = n_from.getRank();
-        int outgoing_number = getOutgoingNodes(n_from).size();
+        float from_rank = all_nodes[getNodeIndexFromLink(parent[i].getUrl())].getRank();
+        int outgoing_number = getOutgoingNodes(all_nodes[getNodeIndexFromLink(parent[i].getUrl())]).size();
         new_rank += from_rank / outgoing_number;
     }
     new_rank = 0.15 + 0.85 * new_rank;
-    float old_rank = n_current.getRank();
+    float old_rank = all_nodes[index_current].getRank();
     //update the rank
-    n_current.updateRank(new_rank);
-
+    all_nodes[index_current].updateRank(new_rank);
     //check converge, threshold current set to 0.1%
-    if (abs(new_rank - old_rank) / old_rank < 0.001)
+    if (float(abs(new_rank - old_rank) / old_rank) < 0.00001)
     {
         //converged, current node's children would not add to queue
         updateHelper(work_queue);
@@ -186,10 +198,11 @@ void Webgraph::updateHelper(queue<Node> work_queue)
     else
     {
         //not converged, all current node's children to queue
-        vector<Node> children = getOutgoingNodes(n_current);
+        vector<Node> children = getOutgoingNodes(all_nodes[index_current]);
+
         for (uint i = 0; i < children.size(); ++i)
         {
-            work_queue.push(children[i]);
+            work_queue.push(getNodeIndexFromLink(children[i].getUrl()));
         }
         updateHelper(work_queue);
     }
