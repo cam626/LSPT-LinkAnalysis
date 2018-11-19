b984f114eb249d2d49676d1ad8c136aeded8140f
AJowett <andrewwjowett@gmail.com>
2018-11-19 11:05:34 -0500

    Adds pg to installed packages

--- server/tests/api/index.js
+++ server/tests/api/index.js
@@ -25,7 +25,6 @@ function testGetDocIds() {
 
 
 function testGetPages() {
-  /*
   apis.getPages([], indexingURL, (resp) => {
     console.log(resp);
     assert(resp.length == 0);
@@ -40,6 +39,5 @@ function testGetPages() {
     console.log(resp);
     assert(resp.length == 0);
   });
-  */
 }
 module.exports = {testGetDocIds, testGetPages};
