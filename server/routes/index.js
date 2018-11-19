186c3e4f4c0e49016a1e90ae22242059ba53298e
Theo Lipeles <Theo@Lipeles.com>
2018-11-19 11:18:24 -0500

    adds error handling for getDocIds

--- server/routes/index.js
+++ server/routes/index.js
@@ -4,17 +4,21 @@ const path = require('path');
 const api = require(path.join(__dirname, '..', '/api'));
 
 router.get('/query', (req, res) => {
-  api.getDocIds(req.body.query, (docObj) => {
-    const docIds = [];
-    for (let i = 0; i < docObj.docs; i++) {
-      docIds.push(docObj[i].docid);
-    }
-    try {
-      api.getPages(docIds, res.send);
-    } catch (error) {
-      res.send([]);
-    }
-  });
+  try {
+    api.getDocIds(req.body.query, (docObj) => {
+      const docIds = [];
+      for (let i = 0; i < docObj.docs; i++) {
+        docIds.push(docObj[i].docid);
+      }
+      try {
+        api.getPages(docIds, res.send);
+      } catch (error) {
+        res.send([]);
+      }
+    });
+  } catch (error) {
+    res.send([]);
+  }
 });
 
 module.exports = router;
