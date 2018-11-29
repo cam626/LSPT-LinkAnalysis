a14b9c64577010db6a841915404655f4c5ca4f75
YoungChul Chun <youngcc3157@gmail.com>
2018-11-28 22:41:53 -0500

    Updated more get functions and edited example file to test the functionalities

--- text_extract.py
+++ text_extract.py
@@ -18,20 +18,47 @@ class TextExtractor:
     Returns all links in the document (hrefs)
     '''
     def getLinksInDocument(self):
-        links = None
+        links = []
+        hyperlinks = list(self.root.iter("a"))
+        for a in hyperlinks:
+            links.append(a.get('href'))
         return links
 
     '''
     Returns all 'header' words (text in h1,h2,h3,h4,h5,etc.) in the document.
     '''
     def getHeaderListOfWords(self):
-        return []
+        header_words = []
+        self.parseHeaderNodes(self.root, header_words)
+        return header_words
+
+    '''
+    parseHeaderNodes recursively takes current node and list of header words for input
+        then strip valid header words that get added to words list.
+    '''
+    def parseHeaderNodes(self, node, header_words):
+        if node.tag in ["h1", "h2", "h3", "h4", "h5", "h6"]:
+            self.getWords(node.text, header_words)
+        for child in node:
+            self.parseHeaderNodes(child, header_words)
 
     '''
     Returns all 'title' words. (not sure what this is)
     '''
     def getTitleListOfWords(self):
-        return []
+        title_words = []
+        self.parseTitleNodes(self.root, title_words)
+        return title_words
+
+    '''
+    parseTitleNodes recursively takes current node and list of title words for input
+        then strip valid title words that get added to words list.
+    '''
+    def parseTitleNodes(self, node, title_words):
+        if node.tag == "title":
+            self.getWords(node.text, title_words)
+        for child in node:
+            self.parseTitleNodes(child, title_words)
 
     '''
     Extract the metadata from the document.
@@ -78,7 +105,10 @@ class TextExtractor:
 def main():
     path = 'Examples/example1.html'
     te = TextExtractor(path)
-    #print te.getListOfWords()
+    print te.getListOfWords()
+    print te.getLinksInDocument()
+    print te.getHeaderListOfWords()
+    print te.getTitleListOfWords()
 
 if __name__== "__main__":
     main()
