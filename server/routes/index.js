40437ed09987373c2bee4b9e2891dc414ab70f94
Theo Lipeles <Theo@Lipeles.com>
2018-11-29 11:39:01 -0500

    adds snippet generation

--- server/routes/index.js
+++ server/routes/index.js
@@ -16,6 +16,19 @@ function randUrl(arr) {
   return arr[Math.floor(Math.random() * arr.length)];
 }
 
+/** Creates a 120 character long snippet from the description and paragraphs
+ * @param {String} description The description if it exists
+ * @param {Array} paragraphs An array of paragraphs that come from database
+ * @return {String} a 120 character long string ending in ... if it's truncated
+ */
+function makeSnippet(description, paragraphs) {
+  let snippet = '';
+  if (description) paragraphs.unshift(description);
+  if (paragraphs && paragraphs.length > 0) {
+    snippet += paragraphs.join('... ');
+  }
+  return snippet.length > 120 ? snippet.slice(0, 197) + '...' : snippet;
+}
 
 router.get('/query', (req, res) => {
   api.getDocIds(req.query.query, randUrl(rankingUrls), (docObj) => {
@@ -25,7 +38,14 @@ router.get('/query', (req, res) => {
       docIds.push(docObj.docs[i].docid);
     }
     api.getPages(docIds, randUrl(indexingUrls), (pages) => {
-      res.send(pages);
+      const processed = pages.map((page) => {
+        return {
+          'title': page.title,
+          'url': page.url,
+          'snippet': makeSnippet(page.description, page.paragraphs)};
+      });
+
+      res.send(processed);
     });
   });
 });
