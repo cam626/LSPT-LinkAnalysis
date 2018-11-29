d13d2754f8026983369a7785ff5177a5ac4d9a1b
Eve <evefangy@gmail.com>
2018-11-29 11:39:18 -0500

    Adds code to allow search bar to search in result page

--- public/views/results.js
+++ public/views/results.js
@@ -27,3 +27,34 @@ Http.onreadystatechange = () => {
 $('document').ready(function() {
   $('#searchbar').val(getQuery());
 });
+
+
+/** Append keypress event to the input box */
+$('document').ready(function() {
+  const input = document.getElementById('searchbar');
+  input.addEventListener('keyup', function(event) {
+    // Cancel the default action, if needed
+    event.preventDefault();
+    // If "Enter" key is pressed
+    if (event.keyCode === 13) {
+      document.getElementById('go-button').click();
+    }
+  });
+});
+
+/** Validate the search query and pass the query to the result page */
+function startSearch() {
+  const search = document.getElementById('searchbar');
+  const query = search.value;
+  console.log(query);
+  if (query.length==0) {
+    console.log('Empty query');
+  } else {
+    console.log('Query: '+query);
+    const url = '/results.html?query='+query;
+    window.location = url;
+  }
+}
+
+
+/* exported startSearch */
